a242bb1be88e9be377934053bcd6d50e91309cc3
gaelo21 <59283256+gaelo21@users.noreply.github.com>
2025-01-06 00:57:30 +0100

    Create  NumberOfDiscIntersections.cpp

---
+++ NumberOfDiscIntersections.cpp
@@ -0,0 +1,20 @@
+int solution(vector<int> &A) {
+    // Implement your solution here
+    int N = A.size();
+    vector<long> left = vector<long>(N);
+    vector<long> right = vector<long>(N);
+    for (int i = 0; i < N; i++) {
+        left[i] = right[i] = i;
+        left[i] -= A[i];
+        right[i] += A[i];
+    }
+    sort(left.begin(), left.end());
+    sort(right.begin(), right.end());
+    int count = 0;
+    for (int i = 0, j = 0; i < N; i++) {
+        while (j < N && right[i] >= left[j]) j++;
+        count += j - i - 1;
+        if (count > 10'000'000) return -1;
+    }
+    return count;
+}
